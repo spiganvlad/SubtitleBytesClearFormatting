897e9bd423c2dd256e9380c5093868d5f8a892c9
spiganvlad <spiganvlad@gmail.com>
2022-10-06 13:18:18 +0300

    Ass cleaner tests

---
+++ SubtitleBytesClearFormattingTest/AssCleanerTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Xunit;
+using SubtitleBytesClearFormatting.Cleaners;
+using SubtitleBytesClearFormattingTest.Helpers;
+
+namespace SubtitleBytesClearFormattingTest
+{
+    public class AssCleanerTests
+    {
+        [Fact]
+        public void DeleteFormattingNullParameter()
+        {
+            byte[] subtitleBytes = null;
+            AssCleaner assCleaner = new();
+
+            void act() => assCleaner.DeleteFormatting(subtitleBytes);
+
+            ArgumentNullException exception = Assert.Throws<ArgumentNullException>(act);
+            Assert.Contains("Ass subtitle bytes cannot be null.", exception.Message);
+        }
+
+        [Fact]
+        public void DeleteFormattingEmptyParameter()
+        {
+            byte[] subtitleBytes = Array.Empty<byte>();
+            AssCleaner assCleaner = new();
+
+            List<byte> resultBytes = assCleaner.DeleteFormatting(subtitleBytes);
+
+            Assert.Empty(resultBytes);
+        }
+
+        [Fact]
+        public void DeleteFormattingReturnCorrectWindowsValue()
+        {
+            byte[] subtitleBytes = BytesLoadHelper.GetFileBytesArray("./TestData/Ass/Cases/AssWindowsCase.ass");
+            List<byte> expectedBytes = BytesLoadHelper.GetFileBytesList("./TestData/Ass/ExpectedResults/AssWindowsCaseResult.txt");
+            AssCleaner assCleaner = new();
+
+            List<byte> resultBytes = assCleaner.DeleteFormatting(subtitleBytes);
+
+            Assert.Equal(expectedBytes, resultBytes);
+        }
+
+        [Fact]
+        public void DeleteFormattingReturnCorrectUnixValue()
+        {
+            byte[] subtitleBytes = BytesLoadHelper.GetFileBytesArray("./TestData/Ass/Cases/AssUnixCase.ass");
+            List<byte> expectedBytes = BytesLoadHelper.GetFileBytesList("./TestData/Ass/ExpectedResults/AssUnixCaseResult.txt");
+            AssCleaner assCleaner = new();
+
+            List<byte> resultBytes = assCleaner.DeleteFormatting(subtitleBytes);
+
+            Assert.Equal(expectedBytes, resultBytes);
+        }
+
+        [Fact]
+        public void DeleteFormattingReturnCorrectMacintoshValue()
+        {
+            byte[] subtitleBytes = BytesLoadHelper.GetFileBytesArray("./TestData/Ass/Cases/AssMacintoshCase.ass");
+            List<byte> expectedBytes = BytesLoadHelper.GetFileBytesList("./TestData/Ass/ExpectedResults/AssMacintoshCaseResult.txt");
+            AssCleaner assCleaner = new();
+
+            List<byte> resultBytes = assCleaner.DeleteFormatting(subtitleBytes);
+
+            Assert.Equal(expectedBytes, resultBytes);
+        }
+
+        [Fact]
+        public async Task DeleteFormattingAsyncReturnCorrectValue()
+        {
+            byte[] subtitleBytes = BytesLoadHelper.GetFileBytesArray("./TestData/Ass/Cases/AssWindowsCase.ass");
+            List<byte> expectedBytes = BytesLoadHelper.GetFileBytesList("./TestData/Ass/ExpectedResults/AssWindowsCaseResult.txt");
+            AssCleaner assCleaner = new();
+
+            List<byte> resultBytes = await assCleaner.DeleteFormattingAsync(subtitleBytes);
+
+            Assert.Equal(expectedBytes, resultBytes);
+        }
+    }
+}
---
+++ SubtitleBytesClearFormattingTest/Helpers/BytesLoadHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SubtitleBytesClearFormattingTest.Helpers
+{
+    public static class BytesLoadHelper
+    {
+        public static byte[] GetFileBytesArray(string path)
+        {
+            using FileStream fs = new(path, FileMode.Open, FileAccess.Read);
+            byte[] fileBytes = new byte[fs.Length];
+            fs.Read(fileBytes, 0, fileBytes.Length);
+            return fileBytes;
+        }
+
+        public static List<byte> GetFileBytesList(string path) => GetFileBytesArray(path).ToList();
+    }
+}
